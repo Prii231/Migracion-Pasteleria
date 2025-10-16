0a1eb67bbc1ca51e937dda1536e10a7457258d08
Prii231 <nicosanchezf@gmail.com>
2025-10-16 00:55:44 -0300

    Segundo commit prueba - Creacion de un Navbar

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React LOCOO fuera venekos
         </a>
       </header>
     </div>
