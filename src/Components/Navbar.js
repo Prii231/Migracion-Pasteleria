75b2cce34a6d4f774f3ebffbdfdd129e86399703
Prii231 <nicosanchezf@gmail.com>
2025-10-16 23:42:56 -0300

    NPM Install + migracion de Navbar Prueba

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,42 +1,57 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function NavbarPrincipal() {
+    const [buscarTermino, setearTermino] = useState('');
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log('Buscando:', buscarTermino);
+    };
+
     return (
         <div className="container">
             <div className="col-lg-12">
                 <nav className="navbar">
                     <div className="container">
-                        <Link className="navbar-brand d-flex align-items-center" to="index.html">
-                            <img src="assets\images\LogoTitle.png" alt="Bootstrap" width="60" height="60"/>
-                                <span className="textoCuerpo ms-3">
-                                    <h3><strong>Pastelería<br />Mil Sabores</strong></h3>
-                                </span>
+                        <Link className="navbar-brand d-flex align-items-center" to="/">
+                            <img src="/assets/images/LogoTitle.png" alt="Bootstrap" width="60" height="60"/>
+                            <span className="textoCuerpo ms-3">
+                                <h3><strong>Pastelería<br />Mil Sabores</strong></h3>
+                            </span>
                         </Link>
-                        <a href="index.html">
+                        
+                        <Link to="/">
                             <button type="button" className="btn botones ms-3">Inicio</button>
-                        </a>
-                        <a href="catalogo.html">
+                        </Link>
+                        
+                        <Link to="/catalogo">
                             <button type="button" className="btn botones ms-3">Productos</button>
-                        </a>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Buscar producto..."
-                                aria-label="Search" />
+                        </Link>
+                        
+                        <form className="d-flex" role="search" onSubmit={handleSearch}>
+                            <input 
+                                className="form-control me-2" 
+                                type="search" 
+                                placeholder="Buscar producto..."
+                                aria-label="Search"
+                                value={buscarTermino}
+                                onChange={(e) => setearTermino(e.target.value)}
+                            />
                             <button className="btn botones" type="submit">Buscar</button>
                         </form>
 
                         <div className="dropdown-hover icono-espaciado">
                             <i className="fa-solid fa-user iconos"></i>
                             <div className="dropdown-content">
-                                <a href="login.html">Iniciar Sesión</a>
-                                <a href="registro.html">Registrarse</a>
-                                <a href="perfil.html">Actualizar Información</a>
+                                <Link to="/login">Iniciar Sesión</Link>
+                                <Link to="/registro">Registrarse</Link>
+                                <Link to="/perfil">Actualizar Información</Link>
                             </div>
                         </div>
 
                         <i className="fa-solid fa-basket-shopping iconos icono-espaciado"></i>
-
                     </div>
                 </nav>
             </div>
