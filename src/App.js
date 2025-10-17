a3b969951ae62529c2cf6cdc77782debcd747799
Prii231 <nicosanchezf@gmail.com>
2025-10-17 02:06:46 -0300

    Primer Avance, todo ok (funciona)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import NavbarPrincipal from './Components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Home from './pages/Home';
 import CatalogoProductos from './Components/Catalogo';
-
-
-=======
 import Home from './Components/Home'; 
 import Footer from './Components/Footer';
->>>>>>> 56c1cc01586531aac5f4da6d29be06430af4dcca
+
 
 function App() {
   return (
