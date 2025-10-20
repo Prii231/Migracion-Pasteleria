83caaba3508c9d0ff34f00c51fa8c36d73e99f21
Nicolas <nicosanchezf@gmail.com>
2025-10-20 20:26:14 -0300

    Commit desde Notebook

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import './App.css';
 import NavbarPrincipal from './Components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CatalogoProductos from './Components/Catalogo';
-<<<<<<< HEAD
 
-
-
-import Home from './Components/Home'; 
-import Footer from './Components/Footer';
-
-=======
 import Home from './Components/Home'; 
 import Footer from './Components/Footer';
 import Carrusel from './Components/Carrusel';
@@ -18,7 +11,7 @@ import Cards from './Components/Card';
 import Login from './Components/InicioSesion';
 import Registro from './Components/Registro';
 import Perfil from './Components/ActualizarPerfil';
->>>>>>> 0d74a6f5bc6a14a6db18d9e4e860ab6e557c1858
+
 
 function App() {
   return (
@@ -29,17 +22,11 @@ function App() {
         </div>
         <Routes>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-          <Route path="/catalogo" element={<CatalogoProductos/>} />
-          <Route path="/login" element={<div><h1>Login</h1></div>} />
-          <Route path="/registro" element={<div><h1>Registro</h1></div>} />
-          <Route path="/perfil" element={<div><h1>Perfil</h1></div>} />
-=======
+
           <Route path="/catalogo" element={<CatalogoProductos />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/registro" element={<Registro />} />
           <Route path="/perfil" element={<Perfil />} />
->>>>>>> 0d74a6f5bc6a14a6db18d9e4e860ab6e557c1858
         </Routes>
         
         <Footer />
